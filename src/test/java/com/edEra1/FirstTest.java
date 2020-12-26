fdc98b9f20c614b82bb90b6dbf83082ecdb39218
ivannaht <ivhontaryk@gmail.com>
2020-12-26 19:37:22 +0200

    Test Click

--- src/test/java/com/edEra1/FirstTest.java
+++ src/test/java/com/edEra1/FirstTest.java
@@ -2,7 +2,13 @@ package com.edEra1;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstTest {
 
@@ -17,6 +23,11 @@ ChromeDriver driver = new ChromeDriver();
 driver.get("https://www.ed-era.com/");
 String title = driver.getTitle();
 Assert.assertTrue(title.equals("EdEra – студія онлайн-освіти"));
+
+        WebElement body = driver.findElement(By.cssSelector("body"));
+        body.findElement(By.cssSelector("[href=\"https://courses.ed-era.com/register\"]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated (By.id ("register")));
 
 driver.quit();
     }
